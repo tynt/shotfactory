aad2a904cc38af80c78a035ca56c4755bce638dc
johann <johann@ca065b8f-b50a-0410-be8c-a9c8883ec1d6>
2007-06-18 14:45:14 +0000

    Delete browser cache for Konqueror.
    git-svn-id: http://svn.browsershots.org/branches/shotfactory-django@1564 ca065b8f-b50a-0410-be8c-a9c8883ec1d6

---
+++ shotfactory04/gui/linux/konqueror.py
@@ -0,0 +1,49 @@
+# browsershots.org - Test your web design in different browsers
+# Copyright (C) 2007 Johann C. Rocholl <johann@browsershots.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+# MA 02111-1307, USA.
+
+"""
+GUI-specific interface functions for X11.
+"""
+
+__revision__ = "$Rev$"
+__date__ = "$Date$"
+__author__ = "$Author$"
+
+
+import os
+import shutil
+from glob import glob
+from shotfactory04.gui import linux as base
+from shotfactory04.inifile import IniFile
+
+
+class Gui(base.Gui):
+    """
+    Special functions for Konqueror.
+    """
+
+    def reset_browser(self):
+        """
+        Delete browser cache.
+        """
+        dotdir = os.path.join(os.environ['HOME'], '.kde')
+        for cachedir in glob(os.path.join(dotdir, 'cache-*')):
+            while os.path.islink(cachedir):
+                cachedir = os.readlink(cachedir)
+            print 'deleting cache', cachedir
+            shutil.rmtree(cachedir)
