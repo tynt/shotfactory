75927aa9543e6abd1d8120a6db43faff1d579fa2
johann <johann@ca065b8f-b50a-0410-be8c-a9c8883ec1d6>
2007-07-01 12:05:04 +0000

    Support for MSIE on Linux, thanks to Andreas Schönfelder.
    git-svn-id: http://svn.browsershots.org/branches/shotfactory-django@1744 ca065b8f-b50a-0410-be8c-a9c8883ec1d6

---
+++ shotfactory04/gui/linux/msie.py
@@ -0,0 +1,50 @@
+# browsershots.org - Test your web design in different browsers
+# Copyright (C) 2007 Johann C. Rocholl <johann@browsershots.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+# MA 02111-1307, USA.
+
+"""
+GUI-specific interface functions for X11.
+"""
+
+__revision__ = "$Rev$"
+__date__ = "$Date$"
+__author__ = "$Author$"
+
+
+import os
+import shutil
+from glob import glob
+from shotfactory04.gui import linux as base
+
+
+class Gui(base.Gui):
+    """
+    Special functions for IEs4Linux.
+    """
+
+    def reset_browser(self):
+        """
+        Delete browser cache.
+        """
+        dotdir = glob(os.path.join(os.environ['HOME'], '.ies4linux', '*',
+            'drive_c', 'windows', 'profiles', '*', '*', 'Temporary Internet Files'))
+        for cachedir in dotdir:
+            while os.path.islink(cachedir):
+                cachedir = os.readlink(cachedir)
+            if os.path.exists(cachedir):
+                print 'deleting cache', cachedir
+                shutil.rmtree(cachedir)
